e584866401ad591f59108029bbe0c605c64d919b
Tony Taylor <73037339+Crimsonsurf07@users.noreply.github.com>
2022-01-30 15:04:29 -0500

    added 10 quiz questions with answers

---
+++ assets/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What Does CSS Stand For?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What Does PHP Stand For?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What Does SQL Stand For?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What Does XML Stand For?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What Does The DOM Stand For",
+    answer: "Document Object Model",
+    options: [
+      "Double On Math",
+      "Digital Order Manipulation ",
+      "Document Object Model",
+      "Document Oriented Method"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What Will This Return? Boolean(5 < 7)",
+    answer: "true",
+    options: [
+      "false",
+      "NaN",
+      "true",
+      "SyntaxError"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which JavaScript label catches all the values, except for the ones specified?",
+    answer: "default",
+    options: [
+      "catch",
+      "label",
+      "try",
+      "default"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which are the correct “if” statements to execute certain code if “x” is equal to 2?",
+    answer: "if(x == 2)",
+    options: [
+      "if(x 2)",
+      "if(x = 2)",
+      "if(x == 2)",
+      "if(x != 2)"
+    ]
+  },
+  {
+    numb: 10,
+    question: "JavaScript Is A ____-side programming language.",
+    answer: "Both",
+    options: [
+      "Server",
+      "Client",
+      "Both",
+      "None"
+    ]
+  },
+];
